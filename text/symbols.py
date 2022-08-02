d66cfc09142da609589d4fb5bf34d05f386d1464
CjangCjengh <101577701+CjangCjengh@users.noreply.github.com>
2022-08-02 21:33:29 +0800

    add japanese cleaners

--- text/symbols.py
+++ text/symbols.py
@@ -4,9 +4,9 @@
 Defines the set of symbols used in text input to the model.
 '''
 _pad        = '_'
-_punctuation = ';:,.!?¡¿—…"«»“” '
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-_letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
+_punctuation = ',.!?'
+_letters = 'AEINOQUabdefghijkmnoprstuvwyz'
+_letters_ipa = 'ʃʧ↓↑ '
 
 
 # Export all symbols:
