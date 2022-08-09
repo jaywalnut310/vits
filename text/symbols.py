4ebc313eecdfed04016618c3dfad055f95e2ac15
CjangCjengh <101577701+CjangCjengh@users.noreply.github.com>
2022-08-09 23:28:32 +0800

    add korean cleaners

--- text/symbols.py
+++ text/symbols.py
@@ -3,14 +3,27 @@
 '''
 Defines the set of symbols used in text input to the model.
 '''
+
+'''# japanese_cleaners
 _pad        = '_'
 _punctuation = ',.!?-'
-_letters = 'AEINOQUabdefghijkmnoprstuvwyz'
-_letters_ipa = 'ʃʧ↓↑ '
+_letters = 'AEINOQUabdefghijkmnoprstuvwyzʃʧ↓↑ '
+'''
+
+'''# japanese_cleaners2
+_pad        = '_'
+_punctuation = ',.!?-'
+_letters = 'AEINOQUabdefghijkmnoprstuvwyzʃʧʦ↓↑ '
+'''
+
+# korean_cleaners
+_pad        = '_'
+_punctuation = ',.!?…~'
+_letters = 'ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㄲㄸㅃㅆㅉㅏㅓㅗㅜㅡㅣㅐㅔ '
 
 
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+symbols = [_pad] + list(_punctuation) + list(_letters)
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
