a8fa53ae2b704a5d92c4830a320984cc964c9c18
Aloento <11802769+Aloento@users.noreply.github.com>
2022-08-18 12:17:16 +0200

    GUI Init

---
+++ CraftVits/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace CraftVits;
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow {
+    public MainWindow() => InitializeComponent();
+}
--- Runtime/test.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-sys.path.append('./')
-
-import cleaner
-
-res = cleaner.japanese_cleaner("どうしてこうなるんだろう。")
-print(res)
