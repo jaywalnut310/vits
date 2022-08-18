bdd873f34ace80ffd344ac333e391f07155ded6b
Aloento <11802769+Aloento@users.noreply.github.com>
2022-08-18 20:03:12 +0200

    移出

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize("craft_vits.py"),
-      compiler_directives={'language_level': "3"})
+setup(ext_modules=cythonize(
+  ["craft_vits.py", "hparams.py", "inference.py", "load_checkpoint.py", "text/symbols.py",
+   "text/cleaner.py", "commons.py", "duration_predictor.py", "stochastic_duration_predictor.py",
+   "residual_coupling_block.py", "posterior_encoder.py", "generator.py", "text_encoder.py",
+   "attentions.py", "modules.py", "transforms.py"],
+  compiler_directives={'language_level': "3"}))
