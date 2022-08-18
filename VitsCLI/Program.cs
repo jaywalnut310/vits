b7c044ab79c9b66fd4a49c62cd80693763361005
Aloento <11802769+Aloento@users.noreply.github.com>
2022-08-18 14:34:24 +0200

    Cleaner 功能实现

--- VitsCLI/Program.cs
+++ VitsCLI/Program.cs
@@ -1,15 +1,73 @@
-﻿using Python.Runtime;
+﻿namespace VitsCLI;
 
-Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", "python38.dll");
-PythonEngine.PythonHome = "C:\\Users\\SoarT\\scoop\\apps\\python38\\current";
-PythonEngine.Initialize();
+using System.CommandLine;
+using System.IO;
+using System.Runtime.InteropServices;
+using Python.Runtime;
 
-using (Py.GIL()) {
-    dynamic sys = Py.Import("sys");
-    sys.path.append("E:\\Codes\\Vits\\Runtime\\CleanerTest");
+internal class Program {
+    private static bool noTTS;
 
-    dynamic clr = Py.Import("cleaner");
-    dynamic res = clr.japanese_cleaner("どうしてこうなるんだろう。");
+    public static async Task<int> Main(string[] args) {
+        var root = new RootCommand("CraftVits CLI");
 
-    Console.WriteLine(res);
+        {
+            var pyLib = "python38.dll";
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                pyLib = "libpython3.8.so";
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                pyLib = "libpython3.8.dylib";
+
+            Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", pyLib);
+            Console.WriteLine("PythonDLL name is: " + pyLib);
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+            var path = Environment.GetEnvironmentVariable("PATH")?.Split(';')
+                .Where(p => !p.Contains("WindowsApps"))
+                .FirstOrDefault(p => File.Exists(Path.Combine(p, "python.exe")));
+
+            if (!string.IsNullOrEmpty(path)) {
+                PythonEngine.PythonHome = path;
+                Console.WriteLine("Set PythonHome to: " + path);
+            }
+        }
+
+        {
+            var pyLoc = new Option<string?>("--PyLoc", "Specifies the PythonHome location of the Python38.");
+
+            root.AddOption(pyLoc);
+            root.SetHandler(dic => PythonEngine.PythonHome = Directory.Exists(dic)
+                ? PythonEngine.PythonHome = dic
+                : throw new DirectoryNotFoundException("PythonHome Not Exists " + dic), pyLoc);
+        }
+
+        {
+            var pyDLL = new Option<string?>("--PyDLL" ,"Specifies the PythonDLL name of the Python38.");
+
+            root.AddOption(pyDLL);
+            root.SetHandler(name => Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", name), pyDLL);
+        }
+
+        {
+            var clean = new Option<bool>("--Clean", "NoTTS, Only clean the Text");
+
+            root.AddOption(clean);
+            root.SetHandler(b => noTTS = b, clean);
+        }
+
+        {
+            var ja = new Argument<string>("ja", () => "吾輩は猫である。名前はまだない", "The Japanese text to be converted.");
+
+            root.Add(ja);
+            root.SetHandler(str => {
+                using var py = new Vits();
+
+                _ = py.Clean(str);
+            }, ja);
+        }
+
+        return await root.InvokeAsync(args);
+    }
 }
---
+++ VitsCLI/Vits.cs
@@ -0,0 +1,29 @@
+﻿namespace VitsCLI;
+
+using Python.Runtime;
+
+public class Vits : IDisposable {
+    private readonly Py.GILState gil;
+    private readonly dynamic clr;
+
+    public Vits() {
+        PythonEngine.Initialize();
+        this.gil = Py.GIL();
+
+        dynamic sys = Py.Import("sys");
+        sys.path.append(AppContext.BaseDirectory);
+
+        this.clr = Py.Import("cleaner");
+    }
+
+    public string Clean(string str) {
+        var res = this.clr.japanese_cleaner(str);
+        Console.WriteLine(res);
+        return res;
+    }
+
+    public void Dispose() {
+        this.gil.Dispose();
+        PythonEngine.Shutdown();
+    }
+}
