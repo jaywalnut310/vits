e3b2be1b168d21a9f46cf90bc7a7d461bd3330a2
Aloento <11802769+Aloento@users.noreply.github.com>
2022-08-18 17:29:31 +0200

    合并

--- inference.py
+++ inference.py
@@ -1,38 +1,13 @@
 import torch
 from torch import nn
 
+from commons import sequence_mask, generate_path
 from duration_predictor import DurationPredictor
 from stochastic_duration_predictor import StochasticDurationPredictor
 from residual_coupling_block import ResidualCouplingBlock
 from posterior_encoder import PosteriorEncoder
 from generator import Generator
 from text_encoder import TextEncoder
-
-
-def sequence_mask(length, max_length=None):
-  if max_length is None:
-    max_length = length.max()
-  x = torch.arange(max_length, dtype=length.dtype, device=length.device)
-  return x.unsqueeze(0) < length.unsqueeze(1)
-
-
-def convert_pad_shape(pad_shape):
-  l = pad_shape[::-1]
-  pad_shape = [item for sublist in l for item in sublist]
-  return pad_shape
-
-
-def generate_path(duration, mask):
-  b, _, t_y, t_x = mask.shape
-  cum_duration = torch.cumsum(duration, -1)
-
-  cum_duration_flat = cum_duration.view(b * t_x)
-  path = sequence_mask(cum_duration_flat, t_y).to(mask.dtype)
-  path = path.view(b, t_x, t_y)
-  path = path - F.pad(path, convert_pad_shape([[0, 0], [1, 0], [0, 0]]))[:, :-1]
-  path = path.unsqueeze(1).transpose(2, 3) * mask
-  return path
-
 
 class SynthesizerInf(nn.Module):
   """
