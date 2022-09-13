c4d46032e4049d5ec96b020e880bcbee84c6635a
Mikhail Zanka <raznem@gmail.com>
2022-09-13 20:06:33 +0200

    Added build.py

---
+++ build.py
@@ -0,0 +1,23 @@
+import numpy
+from Cython.Build import cythonize
+
+modules = [
+    "vits/monotonic_align/core.pyx",
+]
+
+extensions = cythonize(modules)
+
+
+def build(setup_kwargs):
+    """
+    This is a callback for poetry used to hook in our extensions.
+    """
+
+    setup_kwargs.update(
+        {
+            # declare the extension so that setuptools will compile it
+            "name": "monotonic_align",
+            "ext_modules": extensions,
+            "include_dirs": [numpy.get_include()],
+        }
+    )
