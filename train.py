490b60abe3978650d4341c0c64dc49ab76287d58
Nikita Grebenyuk <nikich340@gmail.com>
2023-01-19 20:34:19 +1000

    Use config boundaries instead of hardcoded

--- train.py
+++ train.py
@@ -67,7 +67,7 @@ def run(rank, n_gpus, hps):
   train_sampler = DistributedBucketSampler(
       train_dataset,
       hps.train.batch_size,
-      [32,300,400,500,600,700,800,900,1000],
+      hps.data.audio_boundaries,
       num_replicas=n_gpus,
       rank=rank,
       shuffle=True)
