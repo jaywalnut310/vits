fd510df9209a832a14f80c6ac9c629cb1c0eca65
Ναρουσέ·μ·γιουμεμί·Χινακάννα <40709280+NaruseMioShirakana@users.noreply.github.com>
2023-03-02 19:07:18 +0800

    Update onnx_models.py

--- onnx_export/onnx_models.py
+++ onnx_export/onnx_models.py
@@ -7,7 +7,6 @@ from torch.nn import functional as F
 import commons
 import onnx_export.onnx_modules as modules
 import attentions
-import monotonic_align
 
 from torch.nn import Conv1d, ConvTranspose1d, AvgPool1d, Conv2d
 from torch.nn.utils import weight_norm, remove_weight_norm, spectral_norm
